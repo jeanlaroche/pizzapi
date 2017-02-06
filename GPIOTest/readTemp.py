7126e0619915c89454d140335b8758c897ce4d20
jean <noEmail>
2017-02-05 22:15:19 -0800

    Cleanup

--- GPIOTest/readTemp.py
+++ GPIOTest/readTemp.py
@@ -33,7 +33,7 @@ def printList(A,f=0):
 			# # N null data read, we're good.
 			# break
 
-def readData():
+def readBinaryData():
 	startRead = 0
 	ii = 1
 	clock = [0]*dataLength
@@ -59,9 +59,12 @@ def readData():
 	# printList(clock)
 	# printList(data)
 
-digitsToNum = {(1,1,1,1,1,1,0):0,(0,1,1,0,0,0,0):1,(1,1,0,1,1,0,1):2,(1,1,1,1,0,0,1):3,(0,1,1,0,0,1,1):4,(1,0,1,1,0,1,1):5,(1,0,1,1,1,1,1):6,(1,1,1,0,0,0,0):7,(1,1,1,1,1,1,1):8,(1,1,1,1,0,1,1):9}
-int2str={0:'0',1:'1'}
-def decodeThis(clock,data):
+# Dictionary with LED segments corresponding to numbers.
+segmentsToNum = {(1,1,1,1,1,1,0):0,(0,1,1,0,0,0,0):1,(1,1,0,1,1,0,1):2,(1,1,1,1,0,0,1):3,(0,1,1,0,0,1,1):4,(1,0,1,1,0,1,1):5,(1,0,1,1,1,1,1):6,(1,1,1,0,0,0,0):7,(1,1,1,1,1,1,1):8,(1,1,1,1,0,1,1):9}
+
+def decodeBinaryData(clock,data):
+# This reads the list of clock and data values, and extracts the series of encoded bits by detecting
+# clock edges.
 	decodedData = []
 	ic = 0
 	N = len(clock)
@@ -75,23 +78,38 @@ def decodeThis(clock,data):
 			ic += 1
 			lastClockUp = ic
 		if not ic < N: break
+		# Clock went low, append bit
 		decodedData.append(dataVal)
-	print "Average samples per clock period: {:.2f}".format(1.0*lastClockUp/len(decodedData))
-	decVal = 0
+	avSamplePerClock = 1.0*lastClockUp/len(decodedData)
+	
+	# decodedData corresponds to the 3x7 segments of the temperature display! It's that simple!
+	# So it's easy to decode with a dictionary. 
+	decVal,heater = 0,0
 	if len(decodedData) == 21:
 		try:
-			# decodedData[4] is probably the heating LED.
+			# decodedData[4] is probably the heater LED.
+			heater = decodedData[4]
 			decodedData[4] = 0
-			D1 = digitsToNum[tuple(decodedData[0:7])]
-			D2 = digitsToNum[tuple(decodedData[7:14])]
-			D3 = digitsToNum[tuple(decodedData[14:])]
+			# You have to go from list to tuple to be able to use the dictionary
+			D1 = segmentsToNum[tuple(decodedData[0:7])]
+			D2 = segmentsToNum[tuple(decodedData[7:14])]
+			D3 = segmentsToNum[tuple(decodedData[14:])]
 			decVal = 100*D1+10*D2+D3
-		except: pass
+		except: 
+			# Exceptions occur when we don't read the bits quite correctly, once in a while.
+			pass
 	
-	# pdb.set_trace()
-	# print "{} trailing zero clocks".format(ic-lastClockUp)
-	
-	return decodedData,decVal
+	return decodedData,decVal,heater,avSamplePerClock
+
+def readTemperature():
+	clock,data,head = readBinaryData()
+	# print "{} zero at head".format(head)
+	# for val in clock: f.write("{} ".format(val))
+	# f.write('\n')
+	# for val in data: f.write("{} ".format(val))
+	# f.write('\n')
+	return decodeBinaryData(clock,data)
+
 	
 if __name__ == "__main__":
     
@@ -101,20 +119,14 @@ if __name__ == "__main__":
 	t1 = time.time()
 	while 1:
 		try:
-			clock,data,head = readData()
-			# print "{} zero at head".format(head)
-			# for val in clock: f.write("{} ".format(val))
-			# f.write('\n')
-			# for val in data: f.write("{} ".format(val))
-			# f.write('\n')
-			decodedData,decData = decodeThis(clock,data)
-			printList(decodedData)
-			printList(decodedData,f)
-			print "Data {:}".format(decData)
+			decodedData,decData,heater,avSampPerClock = readTemperature()
+			# printList(decodedData)
+			# printList(decodedData,f)
+			print "Data {:} -- Heater: {} -- Av. samp. per clock {:.2f}".format(decData,heater,avSampPerClock)
 			t2 = time.time()
 			# print "Elapsed {:.3f}s".format(t2-t1)
 			t1=t2
-			time.sleep(1)
+			time.sleep(.5)
 		except KeyboardInterrupt: break
 		
 	GPIO.cleanup(clockGPIO)
