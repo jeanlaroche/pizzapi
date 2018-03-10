8b0290b716b33eeb7ba0670874a2ae9f2ef30a20
jean <you@example.com>
2018-03-09 22:52:10 -0800

    BUg fix

--- piTherm/heaterControl.py
+++ piTherm/heaterControl.py
@@ -138,13 +138,13 @@ class heaterControl(object):
         def readOutsideTemp():
             while self.stopNow == 0:
                 try:
-                    Str = urllib2.urlopen("http://hottub.mooo.com/airTemp").read()
+                    Str = urllib2.urlopen("http://hottub.mooo.com/airTemp",timeout=3).read()
                     Dict = json.loads(Str)
                     self.outsideTemp = Dict['outsideTemperature']
                     self.outsideHum = Dict['humidity']
                     self.maxAirTemp = Dict['maxAirTemp']
                     self.minAirTemp = Dict['minAirTemp']
-                    Str = urllib2.urlopen("http://lightsjl.mooo.com/_getData").read()
+                    Str = urllib2.urlopen("http://lightsjl.mooo.com/_getData",timeout=3).read()
                     Dict = json.loads(Str)
                     if self.lightOn != Dict['lightStatus']:
                         self.lightOn = Dict['lightStatus']
@@ -332,9 +332,9 @@ class heaterControl(object):
         logging.info("ON LIGHT ON %d",self.lightOn)
         self.lightOn = 1-self.lightOn
         if self.lightOn:
-            Dict = json.loads(urllib2.urlopen("http://lightsjl.mooo.com/_LightOn").read())
+            Dict = json.loads(urllib2.urlopen("http://lightsjl.mooo.com/_LightOn",timeout=3).read())
         else:
-            Dict = json.loads(urllib2.urlopen("http://lightsjl.mooo.com/_LightOff").read())
+            Dict = json.loads(urllib2.urlopen("http://lightsjl.mooo.com/_LightOff",timeout=3).read())
         logging.info("URL RETURN %s",Dict)
         self.lightOn = Dict["lightStatus"]
         self.drawButtons()
