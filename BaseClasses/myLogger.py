59272d7ef3b3ef3caf9779c5230612b7cd88e591
jean <you@example.com>
2018-03-10 07:51:44 -0800

    Improved logger

--- BaseClasses/myLogger.py
+++ BaseClasses/myLogger.py
@@ -1,10 +1,15 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from logging import StreamHandler
 
 defaultFormat = '%(asctime)s - %(filename)s / %(funcName)s- %(levelname)s - %(message)s'
 defaultFormat = '%(asctime)s: %(message)s'
+smallFormat = '%(message)s'
+mstr = None
+lastMessage = ''
 
 def setLogger(fileName,mode='a',level=logging.INFO,format=defaultFormat):
+    global mstr
     logger = logging.getLogger()
     logger.setLevel(level)
     ch = RotatingFileHandler(fileName,mode=mode,maxBytes=1024*1024,backupCount=2)
@@ -13,7 +18,23 @@ def setLogger(fileName,mode='a',level=logging.INFO,format=defaultFormat):
     # add formatter to ch
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    # logger.removeHandler(logger.handlers[0])
+    # This is so I can remember the last message
+    mstr = myStream()
+    ch2 = StreamHandler(mstr)
+    ch2.setLevel(level)
+    formatter2 = logging.Formatter(smallFormat)
+    ch2.setFormatter(formatter2)
+    logger.addHandler(ch2)
+
+    
+class myStream(object):
+    def __init__(self):
+        pass
+    def write(self,thisString):
+        global lastMessage
+        lastMessage = thisString.strip()
+    def flush(self):
+        pass
 
 # import logging
 # from logging.handlers import RotatingFileHandler
