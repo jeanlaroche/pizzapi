8b08471bc1f8e2038dc93f5fe2e34d7f9f1ab339
jean <you@example.com>
2018-03-24 10:26:53 -0700

    Improved

--- piCharger/charger.py
+++ piCharger/charger.py
@@ -11,18 +11,33 @@ app = Flask(__name__)
 
 
 class Charger(Server):
+    
+    oscFreqHz = 10000
     def __init__(self):
         myLogger.setLogger('charger.log',mode='a')
         logging.info('Starting pigpio')
         self.pi = pigpio.pi() # Connect to local Pi.
         self.pi.set_mode(outGPIO, pigpio.OUTPUT)
         self.pi.set_pull_up_down(outGPIO, pigpio.PUD_UP)
-        self.pi.set_PWM_frequency(outGPIO,2000)
+        self.pi.set_PWM_frequency(outGPIO,self.oscFreqHz)
         self.pi.set_PWM_dutycycle(outGPIO,128)
         #self.pi.write(outGPIO,1)
 
     def setDutyCycle(self,ratio):
         self.pi.set_PWM_dutycycle(outGPIO,int(255*(ratio)))
+        
+    def glow(self):
+        def doGlow():
+            while 1:
+                for a in range(0,512):
+                    if a>=256: a = 511-a
+                    charger.setDutyCycle(a/256.)
+                    #print('{:.2f}'.format(a/256.))
+                    time.sleep(0.002)
+        t = threading.Thread(target=doGlow)
+        t.daemon = True
+        t.start()
+
 
 
 @app.route('/favicon.ico')
@@ -53,6 +68,7 @@ charger = Charger()
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8080, debug=True, threaded=False, use_reloader=False)
     #app.run(host='0.0.0.0', port=8080, debug=True, threaded=False, use_reloader=False)
+    charger.glow()
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False, use_reloader=False)
 
     # while 1:
