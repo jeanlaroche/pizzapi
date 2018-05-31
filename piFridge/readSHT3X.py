8855e3de44a4ab1222dc57f8bdf17a814fe68891
jean <you@example.com>
2018-05-30 23:43:54 -0700

    Add required functions and regulation loop

--- piFridge/readSHT3X.py
+++ piFridge/readSHT3X.py
@@ -1,33 +1,79 @@
 import pigpio
 from struct import unpack
 import time
+import json
+import os
 import threading
 from BaseClasses.baseServer import Server
 import logging
 from BaseClasses import myLogger
 from flask import Flask, render_template, request, jsonify, send_from_directory
+from BaseClasses.utils import myTimer
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
 tempGPIO = 18
-humGPIO = 19
+humiGPIO = 19
 
 class FridgeControl(Server):
     
-    oscFreqHz = 320*4
-    pwmRange = 1024
-    target = 255
+    temp = 0
+    humi = 0
+    targetTemp = 52
+    targetHumi = 70
+    tempDelta = 3
+    humiDelta = 5
+    
+    fridgeStatus = 0
+    humidiStatus = 0
+    
+    jsonFile = '.params.json'
+    
     def __init__(self):
         myLogger.setLogger('fridge.log',mode='a')
         logging.info('Starting pigpio')
         self.pi = pigpio.pi() # Connect to local Pi.
         self.pi.set_mode(tempGPIO, pigpio.OUTPUT)
-        self.pi.set_mode(humGPIO, pigpio.OUTPUT)
+        self.pi.set_mode(humiGPIO, pigpio.OUTPUT)
         self.handle = self.pi.i2c_open(1, 0x44)
+        self.stopNow = 0
+        if os.path.exists(self.jsonFile):
+            with open(self.jsonFile) as f:
+                data = json.load(f)
+                self.targetTemp = data['targetTemp']
+                self.targetHumi = data['targetHumi']
         
+        def mainLoop():
+            while self.stopNow==0:
+                try:
+                    self.regulate()
+                except:
+                    pass
+                time.sleep(1)
+            logging.info('Exiting regulate loop')
+            
+        self.timerThread = threading.Thread(target=mainLoop)
+        self.timerThread.daemon = True
+        self.timerThread.start()
+        
+    def writeJson(self):
+        with open(self.jsonFile,'w') as f:
+            json.dump({'targetTemp':self.targetTemp,'targetHumi':self.targetHumi},f)
+    
     def stop(self):
+        self.stopNow=1
+        logging.info("Disconnecting from pigpio")
         self.pi.stop()
+        
+    def setTargetTemp(self,targetTemp):
+        self.targetTemp = targetTemp
+        self.writeJson()
+
+    def setTargetHumi(self,targetHumi):
+        self.targetHumi = targetHumi
+        self.writeJson()
         
     def read_SHT(self,timeOutS=1):
         # I write the raw device as that's easier. This is for no clock stretching.
@@ -49,11 +95,37 @@ class FridgeControl(Server):
         humi = unpack('>H',data[3:5])[0]
         humi = 100.*humi/65535.
         return temp,humi
+        
+    def regulate(self):
+        self.temp,self.humi = self.read_SHT()
+        logging.info("temp: %.2f humid %.2f%%",self.temp,self.humi)
+        if self.temp < self.targetTemp - self.tempDelta:
+            # Turn fridge off
+            if self.fridgeStatus: logging.info("Turning cooling off")
+            self.fridgeStatus = 0
+        if self.temp > self.targetTemp + self.tempDelta:
+            # Turn fridge on
+            if self.fridgeStatus == 0: logging.info("Turning cooling on")
+            self.fridgeStatus = 1
+        if self.humi < self.targetHumi - self.humiDelta:
+            # Turn humidifier on
+            if self.humidiStatus == 0: logging.info("Turning humidifier on")
+            self.humidiStatus = 1
+        if self.humi > self.targetHumi + self.humiDelta:
+            # Turn humidifier off
+            if self.humidiStatus: logging.info("Turning humidifier off")
+            self.humidiStatus = 0
+        self.pi.write(tempGPIO,self.humidiStatus)
+        self.pi.write(humiGPIO,self.humidiStatus)
 
 
 if __name__ == "__main__":
     fc = FridgeControl()
-    for ii in range(100):
-        temp,humi = fc.read_SHT()
-        print "Temp: {:.2f}F Humi {:.2f}%".format( temp, humi)
+    fc.setTargetTemp(40)
+    fc.setTargetHumi(90)
+    time.sleep(10)
+    # for ii in range(100):
+        # temp,humi = fc.read_SHT()
+        # print "Temp: {:.2f}F Humi {:.2f}%".format( temp, humi)
     fc.stop()
+    time.sleep(1)
