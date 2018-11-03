7a120baff3560e24e0694ac70e24c04ea69bbd5b
jean <you@example.com>
2018-11-03 11:21:35 -0700

    Get fix to day from pi-04

--- BaseClasses/utils.py
+++ BaseClasses/utils.py
@@ -2,7 +2,7 @@ import logging
 import datetime
 import time, threading
 
-weekDays = {1:"Mon",2:"Tue",3:"Wed",4:"Thu",5:"Fri",6:"Sat",7:"Sun"}
+weekDays = {0:"Mon",1:"Tue",2:"Wed",3:"Thu",4:"Fri",5:"Sat",6:"Sun"}
 allDays = ''.join(weekDays.values())
 
 class myTimer(object):
