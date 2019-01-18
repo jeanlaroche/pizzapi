a7d8dbf62d941c5be1392c661352ecf9119365e1
jean <you@example.com>
2019-01-17 20:30:59 -0800

    Add duration to send text

--- piGate/sendText.py
+++ piGate/sendText.py
@@ -5,12 +5,20 @@ import time
 account = "AC61f8be00513231ed9b5301e3593d87eb"
 token = "fee568ebe5a55fda904670a0fbcc60f1"
 
-
-def sendText(body,nRepeat=1,pause=0):
-    # Sends a text to my phone number using twilio
-    client = Client(account, token)
-    def doit():
-        for i in range(nRepeat):
-            client.messages.create(to="8312957882", from_="8317775381", body=body)
-            time.sleep(pause)
-    threading.Thread(target=doit).start()
+class sendText(object):
+    stopTimeS = 0
+    def  __init__(self):
+        pass
+        
+    def sendText(self, body, pause=0, durationS = 1):
+        # Sends a text to my phone number using twilio
+        client = Client(account, token)
+        def doit():
+            now = time.time()
+            self.stopTimeS = now+durationS
+            while now < self.stopTimeS:
+                #client.messages.create(to="8312957882", from_="8317775381", body=body)
+                print "Send text"
+                time.sleep(pause)
+                now = time.time()
+        threading.Thread(target=doit).start()
