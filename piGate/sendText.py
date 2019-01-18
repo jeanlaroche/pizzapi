fdc2f505caf17c6d3fe86caefcfef4f0c8e4185e
Jean Laroche <jlaroche@uaudio.com>
2019-01-17 17:26:05 -0800

    Add repeat and pause

--- piGate/sendText.py
+++ piGate/sendText.py
@@ -1,28 +1,16 @@
 from twilio.rest import Client
 import threading
+import time
 
 account = "AC61f8be00513231ed9b5301e3593d87eb"
 token = "fee568ebe5a55fda904670a0fbcc60f1"
 
 
-def repeatFunc(delayS,nRepeats=-1):
-    def innerDec(some_function):
-        def wrapper(*args):
-            some_function(*args)
-            wrapper.n += 1
-            if wrapper.stopNow == 0 and (nRepeats == 0 or wrapper.n < nRepeats):
-                threading.Timer(delayS,wrapper,args).start()
-        wrapper.stopNow = 0
-        wrapper.n=0
-        return wrapper
-    return innerDec
-
-
-def sendText(body):
+def sendText(body,nRepeat=1,pause=0):
     # Sends a text to my phone number using twilio
     client = Client(account, token)
-    client.messages.create(to="8312957882", from_="8317775381", body=body)
-
-@repeatFunc(2,4)
-def test():
-    print("foo")
+    def doit():
+        for i in range(nRepeat):
+            client.messages.create(to="8312957882", from_="8317775381", body=body)
+            time.sleep(pause)
+    threading.Thread(target=doit).start()
