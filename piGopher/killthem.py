07c9f47554b430331ae5909ae4f88daf3a1d7847
jean <you@example.com>
2019-06-06 22:58:20 -0700

    Continue implementing the killer

--- piGopher/killthem.py
+++ piGopher/killthem.py
@@ -9,28 +9,45 @@ from BaseClasses import myLogger
 import numpy as np
 from readADS1015 import *
 
-outGPIO = 18
+relayGPIO = 18      # GPIO used to control the relay
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+status_waiting       = 0
+status_tripped       = 1
+status_off       = 2
 
 class Zapper(Server):
     
     paramFile = 'params.json'
     
     def __init__(self):
-        myLogger.setLogger('zapper.log',mode='a',dateFormat="%H:%M:%S")
-        logging.info('Starting pigpio')
+        myLogger.setLogger('zapper.log',mode='a',dateFormat="%H:%M:%S",level=logging.WARNING)
+        logging.warning('Starting pigpio')
         self.pi = pigpio.pi() # Connect to local Pi.
-        self.pi.set_mode(outGPIO, pigpio.OUTPUT)
-        self.counter = 0
-        self.gain = 2
-        self.Vots = [6.144,4.096,2.048,1.024,0.512,0.256]
+        self.pi.set_mode(relayGPIO, pigpio.OUTPUT)
+        self.pi.write(relayGPIO,0)
+        
+        # The gain of the selectable gain in the AD converter. Between 0 and 5.
+        self.gain = 0
+        # Max input voltage for each gain value.
+        self.maxVolts = [6.144,4.096,2.048,1.024,0.512,0.256]
+        # The output voltage is a 12 bit signed integer.
         self.scale = np.power(2.,-11)
+        # Calibration voltage (voltage when nobody touches the probe)
         self.calibV = 0
+        # Current measured voltage.
         self.curV = 0
+        # self.threshFactor * self.calibV is the threshold below which we trigger the relay
+        self.threshFactor = .9 
+        # The relay is on for this amount of time.
+        self.relayOnTimeS = 4
+        # Status. 
+        self.status = status_waiting
+        # Last time tripped
+        self.lastTripTime = ''
         
         YL_40=0x48
         self.handle = self.pi.i2c_open(1, YL_40, 0)
@@ -49,7 +66,7 @@ class Zapper(Server):
         # for a in range(0,2):
             # # Massaging: get the mean of 512 values, then use the 1024 last one for the feedback and many more for the display
             # values = readValues(chan=a,gain=self.gain,numVals=100,verbose=0)[0]
-            # meanVal = 1.*np.mean(values)*self.Vots[self.gain]
+            # meanVal = 1.*np.mean(values)*self.maxVolts[self.gain]
             # self.outputVHist[a] = np.roll(self.outputVHist[a],1)
             # self.outputVHist[a][0]= meanVal
             # self.outputV[a]=np.mean(self.outputVHist[a][0:self.nMean])
@@ -69,48 +86,71 @@ class Zapper(Server):
                 cnt += 1
                 num_read, data = pi.i2c_read_device(h, 2)
                 value = (unpack('>h',data[0:2])[0])>>4
-                print "val {}".format(value)
+                #print "val {}".format(value)
         t = threading.Thread(target=doLoop)
         t.daemon = True
         t.start()
         
         
     def watchForGopher(self):
-        fullRange = 2048.
+        
+        def sendData(str):
+            try:
+                socketio.emit('currentValues', {'status': str,'curV':self.curV,'calibV':self.calibV,'lastTripped':self.lastTripTime})
+            except:
+                pass
         
         def doLoop():
             while 1:
                 #self.readADInputs()
-                values = readValues(chan=0,gain=self.gain,numVals=10,verbose=0)[0]
-                meanVal = 1.*np.mean(values)*self.scale*self.Vots[self.gain]
-                print "val {}".format(meanVal)
-                str = 'Nothing'
+                values = readValues(chan=0,gain=self.gain,numVals=1000,verbose=0)[0]
+                meanVal = 1.*np.mean(values)*self.scale*self.maxVolts[self.gain]
+                #print "val {}".format(meanVal)
+                if self.status == status_waiting: str = 'Status: waiting... '
+                if self.status == status_tripped: str = 'Status: tripped... '
+                if self.status == status_off: str = 'Status: off... '
                 self.curV = meanVal
-                AOut = 0
-                VMax = 0
-                AMax = 0
-                control = 0
-                try:
-                    socketio.emit('currentValues', {'data': str,'curV':self.curV,'calibV':self.calibV,'VMax':VMax,'AMax':AMax,'Control':control})
-                except:
-                    pass
-                time.sleep(.3)
+                sendData(str)
+                if self.curV < self.calibV * self.threshFactor and self.status == status_waiting:
+                    # Turn the relay on
+                    logging.warning('Triggering relay. V = %.2f Threshold = %.2f',self.curV,self.calibV * self.threshFactor)
+                    self.status = status_tripped
+                    self.pi.write(relayGPIO,1)
+                    sendData('Triggering relay')
+                    self.lastTripTime = 'last tripped: {}'.format(time.asctime(time.localtime()))
+                    # Since we're on only for a little while, we can just sleep here, then reset the relay
+                    time.sleep(self.relayOnTimeS)
+                    self.pi.write(relayGPIO,0)
+                    
+                time.sleep(.1)
         t = threading.Thread(target=doLoop)
         t.daemon = True
         t.start()
         
     def calibrate(self):
-        logging.info("Calibrating")
+        logging.warning("Calibrating")
         self.calibV = self.curV
+    
+    def reArm(self):
+        logging.warning("Resetting, re-arming, curV %.2f calibV %.2f",self.curV,self.calibV)
+        self.status = status_waiting
+    
+    def turnOn(self,onOff):
+        if onOff:
+            if self.status == status_off: 
+                self.status = status_waiting
+        else:
+            self.status = status_off
         
     def saveParams(self):
         with open(self.paramFile,'w') as f:
-            json.dump({'calibV':self.calibV},f)
+            json.dump({'calibV':self.calibV, 'gain':self.gain},f)
     def loadParams(self):
         try:
             with open(self.paramFile) as f:
                 D = json.load(f)
                 self.calibV = D['calibV']
+                self.gain = D['gain']
         except:
             pass
         
@@ -157,17 +197,15 @@ def setGain(arg1):
     #print "SET GAIN {}".format(zapper.gain)
     zapper.saveParams()
 
-@socketio.on('setTargetA')
-def setTargetA(arg1):
-    #zapper.targetOutA = int(arg1['data']*zapper.posToAdcA)
-    zapper.saveParams()
+@socketio.on('reArm')
+def reArm():
+    zapper.reArm()
 
 @socketio.on('TurnOnOff')
 def turnOnOff(arg1):
     print "TURNING ON?OFF {}".format(arg1['data'])
     val = int(arg1['data'])
-    #if val in [0,1]: zapper.turnOn(val)
-    #else: zapper.resetMwHour()
+    zapper.turnOn(val)
 
 @socketio.on('Calib')
 def Calib(arg1):
