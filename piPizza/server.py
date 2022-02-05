83251e2f46f4b8642cdd1da8451baf2d245b0fe7
jean <you@example.com>
2022-02-05 06:51:49 +0000

    Add server

---
+++ piPizza/server.py
@@ -0,0 +1,56 @@
+from BaseClasses.baseServer import Server
+from ReadTemps import Temps
+import pigpio
+
+#from BaseClasses.utils import *
+from flask import Flask
+
+app = Flask(__name__)
+
+RelayTop    = 14
+RelayBottom = 15
+
+class PizzaServer(Server):
+    def __init__(self):
+        super().__init__()
+        self.Temps = Temps()
+        self.pi = pigpio.pi()  # Connect to local Pi.
+        self.pi.set_mode(RelayTop, pigpio.OUTPUT)
+        self.pi.set_mode(RelayBottom, pigpio.OUTPUT)
+        self.pi.write(RelayTop, 0)
+        self.pi.write(RelayBottom, 0)
+
+    def __delete__(self):
+        self.pi.stop()
+
+@app.route('/favicon.ico')
+def favicon():
+    return server.favicon()
+
+
+@app.route("/reboot")
+def reboot():
+    server.reboot()
+    return ('', 204)
+
+
+@app.route('/kg')
+def kg():
+    server.kg()
+    return ('', 204)
+
+
+# return index page when IP address of RPi is typed in the browser
+@app.route("/")
+def Index():
+    return server.Index()
+
+
+@app.route("/funcName/<int:param1>/<int:param2>")
+def funcName(param1, param2):
+    return jsonify(param1=param1, param2=param2)
+if __name__ == "__main__":
+    server = PizzaServer()
+
+    #app.run(host='127.0.0.1', port=8080, debug=True, threaded=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False, use_reloader=False)
